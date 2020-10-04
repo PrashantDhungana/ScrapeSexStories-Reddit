6f21d1ef6b8c675b35aea4f1020f3ac8539b02a0
PrashantDhungana <sharma.prashant2000@gmail.com>
2020-10-04 06:45:41 -0400

    Removed the print statements

--- Scrape.py
+++ Scrape.py
@@ -16,22 +16,17 @@ newData=data['data']['children']
 
 #function for creating files
 def createFiles(path,newtiile,title,body):
-	print(path)
-	print(title)
-
+	
 	f = open(path+"/"+newtitle, "w")
 	f.write("\t\t\t\t\t\t\t\t\t\t"+title+"\n\n")
 	f.write(body)
 	f.close()
-	print("File {0} created successfully!!!".format(title))
 
 for story in newData:
 	title=story['data']['title']
-	print("Getting Title")
 	newtitle=title.replace(" ","_")[:25] #Make Title without any space
 	link=story['data']['permalink']
 	gotolink=requests.get('https://api.reddit.com/'+link,headers=header)
-	print("Visiting link")
 
 	individualData=gotolink.json()
 	#Saving file to json
@@ -46,29 +41,21 @@ for story in newData:
 		tag="None"
 	else:	
 		tag=individualData[0]['data']['children'][0]['data']['link_flair_text'].split()[0]
-	print("**********************The title is {0} **************************".format(title))
-	print("**********************The tag is {0} **************************".format(tag))
 	Date=datetime.datetime.now()
 	dirName=str(Date.year)+"_"+str(Date.month)+"_"+str(Date.day)
 
 	workingDir=os.getcwd() #/home/kaliDesktop/project
-	print("This is a Working dir YOO!! {0}".format(workingDir))
 	fullpath=workingDir+"/"+dirName+"/"+tag
-	print("This is the fullpath hehehaha {0}".format(fullpath))
 
 	if(os.path.isdir(dirName)):
-		print("Today folder already exists")
 		if(os.path.isdir(fullpath)):
-			print("Creating files from fullpath")
 			createFiles(fullpath,newtitle,title,body)
 		else:
 			tag=dirName+"/"+tag
 			os.mkdir(tag)
-			print("Making dir for the tag {0}".format(tag))
 			createFiles(fullpath,newtitle,title,body)
 
 				
 	else:
 		os.makedirs(dirName+"/"+tag)
-		print("Created multiple dirs till tag")
 		createFiles(fullpath,newtitle,title,body)
