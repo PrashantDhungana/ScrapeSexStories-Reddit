4f5257526dff06525a274f7cb37776b22e835b3b
Prashant Dhungana <prashantsharma.dhungana@texasintl.edu.np>
2020-10-05 11:47:28 +0545

    Added new print statements

--- Scrape.py
+++ Scrape.py
@@ -8,6 +8,7 @@ import json
 header={'User-agent':'my bot 0.1'}
 
 r = requests.get('https://api.reddit.com/r/sexstories',headers=header)
+print("Reaching home !!!")
 
 data=r.json()
 newData=data['data']['children']
@@ -21,30 +22,40 @@ def createFiles(path,newtitle,title,body):
 	f.write("\t\t\t\t\t\t\t\t\t\t"+title+"\n\n")
 	f.write(body)
 	f.close()
+	print("Eggs fertilized Successfully !!!")
 
 for story in newData:
+	print("Checking out the Body !!!")
+
 	title=story['data']['title']
 	newtitle=title.replace(" ","_")[:25] #Make Title without any space
 	link=story['data']['permalink']
 	gotolink=requests.get('https://api.reddit.com/'+link,headers=header)
+	print("Opening Clothes !!!")
 
 	individualData=gotolink.json()
+	
 	#Saving file to json
-	with open('output.json', 'a') as json_file:
-		json.dump(individualData, json_file)
+	# with open('output.json', 'a') as json_file:
+	# 	json.dump(individualData, json_file)
 
 	body=individualData[0]['data']['children'][0]['data']['selftext']
 
+
 	#Get tag Flair
+	print("Performing foreplay !!!!")
+
 	tag=individualData[0]['data']['children'][0]['data']['link_flair_text']
 	if tag is None:
 		tag="None"
 	else:	
 		tag=individualData[0]['data']['children'][0]['data']['link_flair_text'].split()[0]
+	
+	print("Watching time !!!")
 	Date=datetime.datetime.now()
 	dirName=str(Date.year)+"_"+str(Date.month)+"_"+str(Date.day)
 
-	workingDir=os.getcwd() #/home/kaliDesktop/project
+	workingDir=os.getcwd() #/home/kali/Desktop/project
 	fullpath=workingDir+"/"+dirName+"/"+tag
 
 	if(os.path.isdir(dirName)):
